65bab4a7c62da84633987c3f4ec98f234cb93f78
AntonioDev <138225195+Antonioneto027@users.noreply.github.com>
2025-05-07 09:20:50 -0300

    Update routes.php

--- Routes/routes.php
+++ Routes/routes.php
@@ -7,17 +7,11 @@ namespace Routes;
 use Controller\TemplateController;
 
 
-/* 
-$routes = [
-    '/aplicandoMvc' => [(new TemplateController())->index()],
-    '/answer' =>[(new TemplateController())->question()] // Verificar o que está causando o erro.
-] */
- 
 
 $routes = [
     '/aplicandoMvc' => [ TemplateController::class, 'index'],
     '/aplicandoMvc/hello' =>[ TemplateController::class, 'hello'],
-    '/aplicandoMvc/save' => [ TemplateController::class, 'save'] // Verificar o que está causando o erro.
+    '/aplicandoMvc/save' => [ TemplateController::class, 'save']  
 ]
 
 
